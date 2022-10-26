24a766211d39a145845eb7d9a524a1a068413202
Elabo <ellaboevans@gmail.com>
2022-10-26 11:58:36 +0000

    Add comment to explain functions

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
+// Selection of classes
+
 const hour = document.querySelector("#hours");
-        const minute = document.querySelector("#minute");
-        const seconds = document.querySelector("#seconds")
-        const amorpm = document.querySelector("#amorpm");
+const minute = document.querySelector("#minute");
+const seconds = document.querySelector("#seconds");
+const amorpm = document.querySelector("#amorpm");
 
-        
-        function newTime (){
-            let date = new Date ()
-            // console.log(date)
-            let hourSpan = date.getHours() % 24;
-            hourSpan = ("0" + hourSpan).slice(-2)
-            let minuteSpan = date.getMinutes();
-            minuteSpan =("0" + minuteSpan).slice(-2)
-            let secondsSpan = date.getSeconds();
-            secondsSpan =("0" + secondsSpan).slice(-2)
+// Display time function
 
+function newTime() {
+    // Date declaration goes here
+    
+  let date = new Date();
+  let hourSpan = date.getHours() % 24;
+  hourSpan = ("0" + hourSpan).slice(-2);
+  let minuteSpan = date.getMinutes();
+  minuteSpan = ("0" + minuteSpan).slice(-2);
+  let secondsSpan = date.getSeconds();
+  secondsSpan = ("0" + secondsSpan).slice(-2);
 
-            // console.log(secondsSpan)
+  // Connecting functions to the DOM
 
-           hour.innerText = hourSpan
-           minute.innerText = minuteSpan
-           seconds.innerText = secondsSpan
+  hour.innerText = hourSpan;
+  minute.innerText = minuteSpan;
+  seconds.innerText = secondsSpan;
 
-           let amorpmSpan
+  //   Am or PM logic goes here
 
-           
-           amorpmSpan = hourSpan >= 0 && hourSpan <=11 ? "AM" : "PM" 
-            amorpm.innerText = amorpmSpan;
-            
-        }
-        setInterval(newTime, 1000)
+  let amorpmSpan;
+
+  amorpmSpan = hourSpan >= 0 && hourSpan <= 11 ? "AM" : "PM";
+  amorpm.innerText = amorpmSpan;
+
+}
+// Invoking function with setInterval
+
+setInterval(newTime, 1000);
